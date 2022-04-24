c2ce4f546966f03d76b767b8a47403394326f865
Shivanshu Pandey <shivanshup1@gmail.com>
2022-04-24 14:21:13 +0530

    fourth

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Aboutus from './components/aboutus/aboutus';
 import Home from './components/home/home';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contactus from './components/contactus/contactus';
 import Blogs from './components/blogs/blog';
-
+import Post from './components/post/post';
 
 function App() {
   return (
@@ -15,10 +15,13 @@ function App() {
       <Router>
         <Header></Header>
         <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path='/aboutus' element={<Aboutus></Aboutus>}></Route>
-          <Route path='/contactus' element={<Contactus></Contactus>}></Route>
-          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/aboutus" element={<Aboutus></Aboutus>}></Route>
+          <Route path="/contactus" element={<Contactus></Contactus>}></Route>
+
+          <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+
+          <Route path=":postSlug" element={<Post />} />
         </Routes>
         <Footer></Footer>
       </Router>
