61cc4763840c108a59a17bb5b7c1c3c33c435fea
Shivanshu Pandey <shivanshup1@gmail.com>
2022-04-24 11:43:27 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
-    <h1>Pandeyji</h1>
+      <Header></Header>
+      <Home />
+      <Footer></Footer>
     </div>
   );
 }
